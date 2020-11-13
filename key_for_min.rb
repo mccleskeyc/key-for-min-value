232fa92fe9bb6cf99269040c4a7a080c027649b7
mccleskeyc <mccleskeyc@gmail.com>
2020-11-13 15:48:21 -0500

    commit tree 9275d7032318ddfc2b48adf45549a74a42cc8145

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_key = name_hash.reduce do |key, value| # set min_key as: "name_hash" hash does the following to elements key and value
+       if key.last > value.last # if the last key is greater than the last value
+        value # then return value
+       else # if not
+        key #return key
+       end
+      end
+        if name_hash == {} # if the hash is empty
+          min_key #return the min_key variable above
+        else # if not
+          min_key.first #return the first in min_key
+        end
 end
